504bcf8bae2382309cf0337f653708b4d8d36b6d
Carlos Arthur <arthurandrade918@gmail.com>
2026-09-05 19:21:21 -0300

    alterando front pra tela de celular

--- Frontend/imdb-passione/app/page.tsx
+++ Frontend/imdb-passione/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-1/3 bor-border p-10 border border-white rounded-md">
-        <h1>Olá meus amigos :D</h1>
-        <h1>Frontend já tá up só falta produzir ele</h1>
+      <div className="w-4/5 p-5 lg:w-1/3 bor-border lg:p-10 border border-white rounded-md">
+        <h1 className="text-sm lg:text-lg">Olá meus amigos :D</h1>
+        <h1 className="text-sm lg:text-lg">Frontend já tá up só falta produzir ele</h1>
       </div>
     </div>
   );
