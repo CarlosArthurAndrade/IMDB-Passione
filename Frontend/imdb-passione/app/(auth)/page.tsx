e918830290f4d09ab7a378af374d4816983c513e
Carlos Arthur <arthurandrade918@gmail.com>
2026-09-07 21:47:49 -0300

    tirando botão de darkmode da prod e removendo as bordas dos inputs

--- Frontend/imdb-passione/app/(auth)/page.tsx
+++ Frontend/imdb-passione/app/(auth)/page.tsx
@@ -7,21 +7,21 @@ export default function Home() {
       <div className="w-4/5 p-5 lg:w-1/3 flex flex-col items-center bg-card box-border lg:p-10 rounded-lg">
         <h1 className="text-xl mb-5 mt-5">Faça seu login!</h1>
         <div className="w-full flex flex-col space-y-2">
-          <div className="w-full bg-input bg-input-border p-3 rounded-lg">
+          <div className="w-full py-3 rounded-lg">
             <p className="">Email:</p>
             <input 
               type="text" placeholder="exemplo@email.com"
-              className="w-full p-2 text-sm bg-input bg-input-border focus:outline-none rounded-lg"
+              className="w-full p-2 text-sm bg-input-border focus:outline-none rounded-lg"
               />
           </div>
-          <div className="w-full bg-input bg-input-border p-3 rounded-lg">
+          <div className="w-full py-3 rounded-lg">
             <div className="w-full flex justify-between">
               <p className="">Senha:</p>
               <a className="text-xs login-link">Esqueci minha senha</a>
             </div>
             <input 
               type="text" placeholder="exemplo@email.com"
-              className="w-full p-2 text-sm bg-input bg-input-border focus:outline-none rounded-lg"
+              className="w-full p-2 text-sm bg-input-border focus:outline-none rounded-lg"
               />
           </div>
           <div className="w-full flex flex-col items-center space-y-4">
@@ -40,7 +40,7 @@ export default function Home() {
               </a>
             </div>
             </div>
-            <ThemeToggle />
+            {/* <ThemeToggle /> */}
         </div>
       </div>
     </div>
